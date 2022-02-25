29f14118ca5f404a43a8a213aef5c5e7451ea7f5
João Vitor Nunes Lobato de Souza <jaolobato85@gmail.com>
2022-02-25 16:40:37 -0300

    Conceitos
    Fazendo Data e Hora aparecerem em tempo real.
    Utilizando a função Date() com os seus métodos.

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,3 +1,56 @@
 class calcController {
     
+
+    constructor(){
+        this._locale = 'pt-BR';
+        this._displayCalEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+        this._currentDate;
+        this.initialize();
+    }
+
+    initialize(){
+
+        this.setTimeDisplayNow();
+        setInterval( () =>{
+        this.setTimeDisplayNow();
+        }, 1000)
+        
+    }
+
+    setTimeDisplayNow(){
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale)
+    }
+
+    get displayTime(){
+        return this._timeEl.innerHTML;
+    }
+    set displayTime(value){
+        return this._timeEl.innerHTML = value
+    }
+
+    get displayDate(){
+        this._dateEl.innerHTML;
+    }
+    set displayDate(value){
+        this._dateEl.innerHTML = value
+    }
+
+    get displayCalc(){
+        return this._displayCalEl.innerHTML;
+    }
+
+    set displayCalc(valor){
+        this._displayCalEl.innerHTML = valor
+    }
+
+    get currentDate(){
+    return new Date();
+    }
+
+    set dataAtual(valor){
+        this.currentDate = valor
+    }
 }
