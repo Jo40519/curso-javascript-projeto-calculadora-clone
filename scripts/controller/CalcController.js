bacf0c260a152469eae31c9d41496d572f3c5c41
João Vitor Nunes Lobato de Souza <jaolobato85@gmail.com>
2022-03-07 16:24:01 -0300

    Update CalcController.js

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -61,10 +61,20 @@ class calcController {
         let result = eval(this._operation.join(''));
 
         this._operation = [result, last];
+
+        this.setLastNumberToDisplay();
     }
 
     setLastNumberToDisplay() {
-        
+        let lastNumber
+
+        for (let i = this._operation.length - 1; i >= 0; i--){
+            if (!this.isOperator(this._operation[i])) {
+                lastNumber = this._operation[i];
+                break;
+            }
+        }
+        this.displayCalc = lastNumber
     }
     addOperation(value){
 
@@ -82,6 +92,8 @@ class calcController {
             } 
             else {
                 this.pushOperation(value)
+
+                this.setLastNumberToDisplay();
             }
         } else {
 
