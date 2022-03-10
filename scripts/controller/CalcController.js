2a142d81d50f36725cd7199a904201d117728202
João Vitor Nunes Lobato de Souza <jaolobato85@gmail.com>
2022-03-09 22:32:58 -0300

    Update CalcController.js

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -153,7 +153,7 @@ class calcController {
             } else {
                 //number
                 let newValue = this.getLastOperation().toString() + value.toString();
-                this.setLastOperation(parseFloat(newValue))
+                this.setLastOperation(newValue)
 
                 // Atualizar Display
 
@@ -171,6 +171,11 @@ class calcController {
 
     addDot() {
         let lastOperation = this.getLastOperation();
+
+        if (typeof lastOperation === 'string' && lastOperation.split('').indexOf('.') > -1) return
+            
+            
+            
         if (this.isOperator(lastOperation) || !lastOperation) {
             this.pushOperation('0.')
         } else {
