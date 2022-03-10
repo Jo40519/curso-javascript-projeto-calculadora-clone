13775fc9907e5ea487a0e01c25a6e5086c36335a
João Vitor Nunes Lobato de Souza <jaolobato85@gmail.com>
2022-03-09 23:40:02 -0300

    Update CalcController.js

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -12,6 +12,7 @@ class calcController {
         this._currentDate;
         this.initialize();
         this.iniitButtonsEvents();
+        this.initKeyBoard();
     }
 
     initialize(){
@@ -25,6 +26,49 @@ class calcController {
         
     }
    
+    initKeyBoard() {
+        document.addEventListener('keyup', e => {
+
+            console.log(e)
+             switch(e.key){
+                case 'Escape':
+                this.clearAll();
+                break;
+                case 'Backspace':
+                this.clearEntry();
+                break;
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '/':
+                    this.addOperation(e.key);
+                    break;
+                case 'Enter':
+                case '=':
+                    this.calc('=');
+                break;
+
+                case '.':
+                case ',':
+                    this.addDot();
+                break;
+
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    this.addOperation(parseInt(e.key));
+                break;
+            }
+        })
+    }
 
     clearAll() {
         
